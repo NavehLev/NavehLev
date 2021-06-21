ff731c9048d42db2e2ff4ccef60346aae3f9d614
navehlev <navehlev@gmail.com>
2021-06-21 09:46:30 +0300

    order controller

--- inbound_controller.js
+++ inbound_controller.js
@@ -47,6 +47,35 @@ let helpers = {
             }
 
         })
+    },
+    convertOrderData: (reqData) => {
+        return new Promise((resolve, reject) => {
+            try {
+                console.log(reqData);
+                data = {
+                    id: reqData.id,
+                    status: reqData.status,
+                    date_created: reqData.date_created,
+                    discount_total: reqData.discount_total,
+                    discount_tax: reqData.discount_tax,
+                    cart_tax: reqData.cart_tax,
+                    total: reqData.total,
+                    total_tax: reqData.total_tax,
+                    order_key: reqData.order_key,
+                    payment_method: reqData.payment_method,
+                    payment_method_title: reqData.payment_method_title,
+                    transaction_id: reqData.transaction_id,
+                    customer_note: reqData.customer_note,
+                    cart_hash: reqData.cart_hash,
+                    number: reqData.number,
+                    activity_id: reqData.meta_data[1].id,
+                }
+                resolve(data);
+            } catch (err) {
+                reject(err);
+            }
+
+        })
     }
 }
 let customer = async (req, res, next) => {
@@ -83,9 +112,21 @@ let lead = async (req, res, next) => {
     }
 }
 
+let order = async (req, res, next) => {
+    try {
+        let data = await helpers.convertOrderData(req.body);
+        console.log(data);
+        res.send("order was added: " + data);
+    } catch (err) {
+        console.log(err);
+        res.send(err.message);
+    };
+}
+
 
 module.exports = {
     customer,
     makat,
     lead,
+    order
 }
